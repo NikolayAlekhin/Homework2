77fc5bff94c12631fafd749de5cdc903b062bcdb
Nikolay Alekhin <kolyan.alekhin@mail.ru>
2022-12-05 12:05:00 +0300

    pers3

---
+++ 1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+
+
+
+Console.WriteLine("Введите число");
+string namberOnerStr = Console.ReadLine() ??"";
+int nambera =  Convert.ToInt32(namberOnerStr);
+if (nambera>99 && nambera<1000)
+{
+     int a = ((nambera/10)%10);
+     int b = ((nambera/10)%10);
+    Console.WriteLine(b);
+}
+
+
+
+
