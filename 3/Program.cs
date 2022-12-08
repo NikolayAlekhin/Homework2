7a7698c7f7a41881261c4088f222280084e653d0
Nikolay Alekhin <kolyan.alekhin@mail.ru>
2022-12-08 12:18:50 +0300

    pers3

---
+++ 3/Program.cs
@@ -0,0 +1,23 @@
+﻿/*   Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+6 -> да
+7 -> да
+1 -> нет */ 
+
+Console.WriteLine("Введите число");
+string namberOnerStr = Console.ReadLine() ??"";
+int nambera =  Convert.ToInt32(namberOnerStr);
+if (nambera<6 & nambera>0)
+{
+    Console.WriteLine( $"{nambera} = нет");
+}    
+if (nambera<8 & nambera>6)
+{
+    Console.WriteLine( $"{nambera} = да");
+    
+}    
+if (nambera<9 & nambera<0)
+{
+    Console.WriteLine( $"{nambera} = ошибка");
+    
+}
